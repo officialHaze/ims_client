624d40633a6b82a350b729612c6375dcfef4e75b
Assassin <moinak.dey8@gmail.com>
2024-06-09 14:40:10 +0530

    Modify the way products are filtered when a product is searched for

--- src/utils/Filter.ts
+++ src/utils/Filter.ts
@@ -2,10 +2,8 @@ import ProductListQueryData from "../interfaces/ProductListQueryData";
 
 export default class Filter {
   public static filterProductListByName(productList: ProductListQueryData[], delimeter: string) {
-    return productList.filter(
-      productObj =>
-        productObj.product_name.toLowerCase().substring(0, delimeter.length) ===
-        delimeter.toLowerCase()
+    return productList.filter(productObj =>
+      productObj.product_name.toLowerCase().includes(delimeter.toLowerCase())
     );
   }
 }
